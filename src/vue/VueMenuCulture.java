3948088757514af9477b4ae6799131cd2622fd3e
Zapolatero <1>
2020-12-16 10:58:45 +0100

    Rennomage de variables

--- src/vue/VueMenuCulture.java
+++ src/vue/VueMenuCulture.java
@@ -10,22 +10,22 @@ import javafx.scene.layout.GridPane;
 
 public class VueMenuCulture extends Scene {
 
-    protected GridPane grilleAvions;
+    protected GridPane grillePrincipale;
     private controler.ControleurPrincipal controleur = null;
     private Label labelCulture;
     private Button boutonRetour;
 
     public VueMenuCulture() {
         super(new GridPane(), 400,400);
-        grilleAvions = (GridPane) this.getRoot();
+        grillePrincipale = (GridPane) this.getRoot();
         this.labelCulture = new Label("Culture");
         this.boutonRetour = new Button("retour");
     }
 
     public void initialiserMenuCulture() {
-        this.grilleAvions.getChildren().clear();
-        this.grilleAvions.add(this.labelCulture, 0, 0);
-        this.grilleAvions.add(this.boutonRetour, 1, 0);
+        this.grillePrincipale.getChildren().clear();
+        this.grillePrincipale.add(this.labelCulture, 0, 0);
+        this.grillePrincipale.add(this.boutonRetour, 1, 0);
         this.boutonRetour.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
