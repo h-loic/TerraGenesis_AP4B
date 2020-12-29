f07466de8934ed22aa0a0f373c48fd99df2844d9
loic <h-loic@orange.fr>
2020-12-29 16:27:43 +0100

    modification interface menu gouverneur

--- src/vue/VueMenuGouverneurs.java
+++ src/vue/VueMenuGouverneurs.java
@@ -6,12 +6,39 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import modele.Gouverneur;
 
 import java.util.ArrayList;
 
 
 public class VueMenuGouverneurs extends Scene {
+
+    /**
+     *  Constante définissant le style des titres de la vue
+     *
+     * @see VueMenuGouverneurs#VueMenuGouverneurs() 
+     * @see VueMenuGouverneurs#initialiserMenuGouverneurs(ArrayList) 
+     */
+    public static final String STYLE_TITRE = " -fx-font-size: 18; -fx-font-weight: bold; -fx-padding: 15px";
+
+    /**
+     *  Constante définissant le style des boutons pour trier de la vue
+     *
+     * @see VueMenuGouverneurs#VueMenuGouverneurs()
+     * @see VueMenuGouverneurs#initialiserMenuGouverneurs(ArrayList)
+     */
+    public static final String STYLE_BOUTONS_TRIER = "-fx-background-color: #25467F; -fx-text-fill: white; -fx-font-size: 12; -fx-font-weight: bold;-fx-min-width: 80;" +
+            "-fx-border-insets: 5px;-fx-background-insets: 5px;";
+
+    /**
+     *  Constante définissant le style des boutons de la vue
+     *
+     * @see VueMenuGouverneurs#VueMenuGouverneurs()
+     * @see VueMenuGouverneurs#initialiserMenuGouverneurs(ArrayList)
+     */
+    public static final String STYLE_BOUTONS = "-fx-background-color: #25467F; -fx-text-fill: white; -fx-font-size: 12; -fx-font-weight: bold;-fx-min-width: 80;";
 
     protected GridPane grillePrincipale;
     protected GridPane grilleGouverneur;
@@ -24,13 +51,17 @@ public class VueMenuGouverneurs extends Scene {
     private Button boutonTrierParDebloque;
 
     public VueMenuGouverneurs() {
-        super(new GridPane(), 400,400);
+        super(new GridPane(), 500,400);
         grillePrincipale = (GridPane) this.getRoot();
 
         this.labelGouverneurs = new Label("Gouverneurs");
+        this.labelGouverneurs.setStyle(STYLE_TITRE);
         this.boutonRetour = new Button("retour");
+        this.boutonRetour.setStyle(STYLE_BOUTONS);
         this.boutonTrierParNom = new Button("Trier par nom");
+        this.boutonTrierParNom.setStyle(STYLE_BOUTONS_TRIER);
         this.boutonTrierParDebloque = new Button("Trier par debloquer");
+        this.boutonTrierParDebloque.setStyle(STYLE_BOUTONS_TRIER);
     }
 
     public void initialiserMenuGouverneurs(ArrayList<Gouverneur> listeGouverneur) {
@@ -40,10 +71,11 @@ public class VueMenuGouverneurs extends Scene {
 
         for (Gouverneur gouverneur : listeGouverneur){
             Label labelNom = new Label(gouverneur.getNom());
-            lignesGouverneur+=2;
+            lignesGouverneur+=1;
             this.grilleGouverneur.add(labelNom, 0, lignesGouverneur);
             if (gouverneur.estDebloque()){
                 Button btnAfficher = new Button("afficher");
+                btnAfficher.setStyle(STYLE_BOUTONS);
                 btnAfficher.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
@@ -53,6 +85,7 @@ public class VueMenuGouverneurs extends Scene {
                 this.grilleGouverneur.add(btnAfficher, 4, lignesGouverneur);
             }else{
                 Button btnDebloque = new Button("debloquer");
+                btnDebloque.setStyle(STYLE_BOUTONS);
                 btnDebloque.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
@@ -61,9 +94,11 @@ public class VueMenuGouverneurs extends Scene {
                     }
                 });
                 this.grilleGouverneur.add(btnDebloque, 4, lignesGouverneur);
+
             }
 
         }
+
         lignesGouverneur+=2;
         this.grillePrincipale.add(this.labelGouverneurs, 0, 0);
         this.grillePrincipale.add(this.boutonTrierParNom, 1, 0);
