a8041441cc57eb9273f641475ff696c17b5ba782
Zapolatero <1>
2020-12-29 21:19:52 +0100

    doc VueAvantPoste

--- src/vue/VueAvantPoste.java
+++ src/vue/VueAvantPoste.java
@@ -1,5 +1,6 @@
 package vue;
 
+import controler.ControleurPrincipal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -11,24 +12,147 @@ import modele.*;
 
 import java.util.HashMap;
 
+/**
+ * <b>
+ *     Vue affichant les informations d'un avant-poste et de le manipuler:
+ *     <ul>
+ *         <li>elle permet de détruire l'avant-poste</li>
+ *         <li>de visualiser les données de ses mines</li>
+ *         <li>de les améliorer</li>
+ *         <li>de les supprimer</li>
+ *         <li>de créer une nouvelle mine</li>
+ *     </ul>
+ *
+ *
+ * </p>
+ *
+ * @see modele.Mine
+ * @see modele.AvantPoste
+ * @author Zapolatero - Louis Pascuzzi
+ * */
 
 public class VueAvantPoste extends Scene {
 
+    /**
+     * La grille sur laquelle sont ajoutés les différents éléments de la vue
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private GridPane grillePrincipale;
+
+    /**
+     * La grille dans laquelle seront affichées les informations concerant l'avant-poste
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private GridPane grilleAvantPoste;
+
+    /**
+     * La grille sur laquelle sont listées les différentes mines de l'avant-poste
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private GridPane grilleMines;
+
+    /**
+     * Scrollpane dans laquelle sera affichée la grille des mines
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private ScrollPane scrollPaneMines;
 
+    /**
+     * Le controleur de l'application, permet à la vue d'intéragir avec les modèles ou avec le navigateur des vues
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private controler.ControleurPrincipal controleur = null;
+
+    /**
+     * Label affichant le nom de l'avant-poste affiché
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private Label labelNom;
+
+    /**
+     * Label affichant les coordonnées de l'avant-poste affiché
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private Label labelCoordonnees;
+
+    /**
+     * Label affichant la somme des bénéfices des mines de l'avant-postes
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private Label labelBenefices;
+
+    /**
+     * Label affichant les messages d'erreur
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private Label labelMessages;
+
+    /**
+     * Identifiant de l'avant-poste affiché
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     * @see AvantPoste#getId()
+     */
     private int idAvantPoste;
+
+    /**
+     * Bouton permettant de retourner au MenuPopulation
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     * @see VueMenuPopulation
+     */
     private Button btnRetour;
+
+    /**
+     * Bouton permettant de détruire l'avant-poste
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     */
     private Button btnDetruire;
+
+    /**
+     * Bouton permettant d'ajouter une mine en allant sur la vue AjouterMine
+     *
+     * @see VueAvantPoste#VueAvantPoste()
+     * @see VueAvantPoste#initialiserVueAvantPoste(AvantPoste)
+     * @see VueAjouterMine
+     */
     private Button btnAjouterMine;
 
+    /**
+     *  Constructeur de VueAvantPoste
+     *  Créé les différents labels, layouts et boutons de la vue
+     *
+     * @see VueAvantPoste#grillePrincipale
+     * @see VueAvantPoste#grilleMines
+     * @see VueAvantPoste#grilleAvantPoste
+     * @see VueAvantPoste#scrollPaneMines
+     * @see VueAvantPoste#grillePrincipale
+     * @see VueAvantPoste#btnRetour
+     * @see VueAvantPoste#btnAjouterMine
+     * @see VueAvantPoste#labelMessages
+     */
     public VueAvantPoste() {
         super(new GridPane(), 800,400);
         this.grillePrincipale = (GridPane) this.getRoot();
@@ -41,6 +165,24 @@ public class VueAvantPoste extends Scene {
         labelMessages = new Label("");
     }
 
+    /**
+     *  Initialise la vue
+     *      <p>
+     *          Affichage des différentes données de l'avant-poste sur les labels
+     *          Créeation des EventHandler des boutons
+     *          Liste des mines de l'avant-poste
+     *      </p>
+     * @param avantPoste AvantPoste dont on veut afficher les données
+     *
+     * @see VueAvantPoste#grillePrincipale
+     * @see VueAvantPoste#grilleMines
+     * @see VueAvantPoste#grilleAvantPoste
+     * @see VueAvantPoste#scrollPaneMines
+     * @see VueAvantPoste#grillePrincipale
+     * @see VueAvantPoste#btnRetour
+     * @see VueAvantPoste#btnAjouterMine
+     * @see VueAvantPoste#labelMessages
+     */
     public void initialiserVueAvantPoste(AvantPoste avantPoste) {
         idAvantPoste = avantPoste.getId();
 
@@ -50,7 +192,7 @@ public class VueAvantPoste extends Scene {
 
         int ligneMine=0;
 
-        //affichage des listes et de leurs données
+        //affichage des mines et de leurs données
         for(Mine mine : avantPoste.getMines()){
             Label labelNomMine = new Label(mine.getNom()+" : ");
             Label labelBenefice = new Label(", benefice/min : "+Double.toString(mine.getBenefice()));
@@ -60,7 +202,6 @@ public class VueAvantPoste extends Scene {
             Button btnAmeliorer = new Button("Améliorer");
             btnAmeliorer.setUserData(mine.getId());
             Button btnDetruire = new Button("Detruire");
-            //btnAfficher.setUserData(mine.getId());
 
             GridPane grilleMine = new GridPane();
             grilleMine.add(labelNomMine, 0, 0);
@@ -71,15 +212,16 @@ public class VueAvantPoste extends Scene {
             grilleMine.add(btnDetruire, 2, 1);
 
             if (mine.getNiveau()>=5 || !mine.isFonctionnelle()){
+                //si la mine a déjà atteint le niveau maximum (5) le bouton pour l'améliorer est désactivé
                 btnAmeliorer.setDisable(true);
             }
 
             btnAmeliorer.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
-                    //controleur.notifierNaviguerAfficherAvPoste((int)((Button)event.getSource()).getUserData());
                     System.out.println("Amélioration Mine");
                     try {
+                        //appelle de la fonction du controleur permettant d'améliorer la mine
                         controleur.notifierAmeliorerMine((int)btnAmeliorer.getUserData(), avantPoste.getId());
                         labelMessages.setVisible(false);
                         labelMessages.setText("");
@@ -95,6 +237,7 @@ public class VueAvantPoste extends Scene {
                 @Override
                 public void handle(ActionEvent event) {
                     System.out.println("Destruction Mine");
+                    //appelle la fonction du controleur permettant détruire la mine
                     controleur.notifierDetruireMine(mine.getId());
                 }
             });
@@ -118,6 +261,7 @@ public class VueAvantPoste extends Scene {
         btnDetruire.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //appel de la fonction du controleur permettant de détruire l'avant-poste
                 controleur.notifierDetruireAvantPoste(idAvantPoste);
             }
         });
@@ -127,19 +271,24 @@ public class VueAvantPoste extends Scene {
             public void handle(ActionEvent event) {
                 System.out.println("Ajouter mine");
                 try {
+                    //appelle la fonction du controleur permettant de naviguer vers la page d'ajout d'une nouvelle mine
                     controleur.notifierNaviguerAjouterMine(idAvantPoste);
                     labelMessages.setVisible(false);
                     labelMessages.setText("");
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
+                    /*affiche les messages d'erreur
+                    * ex : si le joueur ne peut pas payer une nouvelle mine dans cet avant-poste
+                    * */
                     labelMessages.setText(e.getMessage());
                     labelMessages.setVisible(true);
                 }
             }
         });
 
+        //ajoute la liste des mines à la scrollpane
         scrollPaneMines.setContent(grilleMines);
-        scrollPaneMines.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPaneMines.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         scrollPaneMines.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
         grilleAvantPoste.add(labelNom,0,0);
@@ -151,9 +300,19 @@ public class VueAvantPoste extends Scene {
         grillePrincipale.add(scrollPaneMines, 0, 1);
         grillePrincipale.add(btnAjouterMine, 0, 2);
         grillePrincipale.add(labelMessages, 0, 3);
+        //cache le label des messages d'erreur
         this.labelMessages.setVisible(false);
     }
 
+
+    /**
+     *  Permet d'affecter un controleur à la vue
+     *
+     * @param controleur
+     *
+     * @see ControleurPrincipal
+     * @see VueAvantPoste#controleur
+     */
     public void setControleur(controler.ControleurPrincipal controleur) {
         this.controleur = controleur;
     }
