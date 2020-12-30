0f4666e624ce596b18ef50c5e00831c1cabdcd62
Zapolatero <1>
2020-12-30 17:47:44 +0100

    génération javaDoc

--- src/vue/VueMenuStatistiques.java
+++ src/vue/VueMenuStatistiques.java
@@ -9,10 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import modele.AvantPoste;
-import modele.Ville;
-
-import java.util.ArrayList;
 
 /**
  * <b>
@@ -25,6 +21,46 @@ import java.util.ArrayList;
  * */
 
 public class VueMenuStatistiques extends Scene {
+
+    /**
+     *  Constante définissant le style des scrollpanes de la vue
+     *
+     * @see VueMenuStatistiques#VueMenuStatistiques()
+     * @see VueMenuStatistiques#initialiserMenuStatistiques(double, double, double, double, double, double)
+     */
+    public static final String STYLE_SCROLLPANE = "-fx-background-color:transparent;";
+
+    /**
+     *  Constante définissant le style permettant de mettre du texte en valeur en le mettant en gras
+     *
+     * @see VueMenuStatistiques#VueMenuStatistiques()
+     * @see VueMenuStatistiques#initialiserMenuStatistiques(double, double, double, double, double, double)
+     */
+    public static final String STYLE_GRAS = "-fx-font-size: 13; -fx-font-weight: bold;";
+
+    /**
+     *  Constante définissant le style des titres de la vue
+     *
+     * @see VueMenuStatistiques#VueMenuStatistiques()
+     * @see VueMenuStatistiques#initialiserMenuStatistiques(double, double, double, double, double, double)
+     */
+    public static final String STYLE_TITRE = " -fx-font-size: 18; -fx-font-weight: bold; -fx-padding: 15px";
+
+    /**
+     *  Constante définissant le style du label des erreurs de la vue
+     *
+     * @see VueMenuStatistiques#VueMenuStatistiques()
+     * @see VueMenuStatistiques#initialiserMenuStatistiques(double, double, double, double, double, double)
+     */
+    public static final String STYLE_ERREUR = "-fx-text-fill: red; -fx-font-size: 13; -fx-font-weight: bold";
+
+    /**
+     *  Constante définissant le style des boutons de la vue
+     *
+     * @see VueMenuStatistiques#VueMenuStatistiques()
+     * @see VueMenuStatistiques#initialiserMenuStatistiques(double, double, double, double, double, double)
+     */
+    public static final String STYLE_BOUTONS = "-fx-background-color: #25467F; -fx-text-fill: white; -fx-font-size: 12; -fx-font-weight: bold;-fx-min-width: 80px";
 
     /**
      * La grille sur laquelle sont ajoutés les différents éléments de la vue
@@ -133,7 +169,7 @@ public class VueMenuStatistiques extends Scene {
      * @see VueMenuStatistiques#btnActualiser
      */
     public VueMenuStatistiques() {
-        super(new GridPane(), 400,400);
+        super(new GridPane(), 600,400);
 
         grillePrincipale = (GridPane) this.getRoot();
         this.labelTemperature = new Label("Température : ");
@@ -196,6 +232,7 @@ public class VueMenuStatistiques extends Scene {
             }
         });
         this.sliderPression.setShowTickLabels(true);
+        this.sliderPression.setMinWidth(250);
 
         //affichage de l'oxygene
         this.sliderOxygene.setMin(100);
@@ -208,6 +245,7 @@ public class VueMenuStatistiques extends Scene {
             }
         });
         this.sliderOxygene.setShowTickLabels(true);
+        this.sliderOxygene.setMinWidth(250);
 
         //affichage du niveau d'eau
         this.sliderEau.setMin(150);
@@ -220,6 +258,7 @@ public class VueMenuStatistiques extends Scene {
             }
         });
         this.sliderEau.setShowTickLabels(true);
+        this.sliderEau.setMinWidth(250);
 
         //affichage de la temperature
         this.sliderTemperature.setMin(200);
@@ -232,6 +271,7 @@ public class VueMenuStatistiques extends Scene {
             }
         });
         this.sliderTemperature.setShowTickLabels(true);
+        this.sliderTemperature.setMinWidth(250);
 
         //affichage de la population
         this.labelPopVal.setText(Double.toString(population));
@@ -239,6 +279,15 @@ public class VueMenuStatistiques extends Scene {
 
         //affichage des finances
         this.labelFinancesVal.setText(String.valueOf(finances));
+        this.labelFinancesVal.setMinWidth(100);
+
+
+        this.labelTemperature.setStyle(STYLE_TITRE);
+        this.labelPression.setStyle(STYLE_TITRE);
+        this.labelOxygene.setStyle(STYLE_TITRE);
+        this.labelEau.setStyle(STYLE_TITRE);
+        this.labelPopulation.setStyle(STYLE_TITRE);
+        this.labelFinances.setStyle(STYLE_TITRE);
 
         //permet de retourner au menu principal
         this.btnRetour.setOnAction(new EventHandler<ActionEvent>() {
@@ -247,6 +296,7 @@ public class VueMenuStatistiques extends Scene {
                 controleur.notifierNaviguerMenuPrincipal();
             }
         });
+        this.btnRetour.setStyle(STYLE_BOUTONS);
 
         //permet d'actualiser les données affichées
         btnActualiser.setOnAction(new EventHandler<ActionEvent>() {
@@ -255,6 +305,7 @@ public class VueMenuStatistiques extends Scene {
                 controleur.majDonneesVues();
             }
         });
+        this.btnActualiser.setStyle(STYLE_BOUTONS);
 
         this.grillePrincipale.add(this.labelTemperature, 0, 0);
         this.grillePrincipale.add(this.sliderTemperature, 1, 0);
