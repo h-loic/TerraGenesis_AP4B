5887952af5de08026ff208b03b184fc7786262d9
Zapolatero <1>
2020-12-30 15:50:44 +0100

    Doc NavigateurDesVues

--- src/vue/NavigateurDesVues.java
+++ src/vue/NavigateurDesVues.java
@@ -4,32 +4,204 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+/**
+ * <b>La classe NavigateurDesVues est le point d’entrée de l’application, elle hérite de la classe Application de la bibliothèque JavaFX </b>
+ * <p>
+ *     C’est elle qui va initialiser les différentes vues de l’application, et leur affecter un contrôleur.
+ *     Elle va également permettre au contrôleur d'accéder aux différentes vues  grâce à des accesseurs.
+ *     Elle permet de naviguer facilement entre les différentes vues en changeant la scène du stage
+ * </p>
+ *
+ * @see Stage
+ * @see controler.ControleurPrincipal
+ * @author Zapolatero - lpascuzzi
+ */
+
 public class NavigateurDesVues extends Application{
 
+    /**
+     * <p>
+     *     Stage de l'application
+     * </p>
+     *
+     * @see Stage
+     */
     private Stage stage;
 
+    /**
+     * <p>
+     *     MenuPrincipal de l'application
+     * </p>
+     *
+     * @see VueMenuPrincipal
+     */
     private VueMenuPrincipal vueMenuPrincipal = null;
+
+    /**
+     * <p>
+     *     Menu montrant les données de la planète
+     * </p>
+     *
+     * @see VueMenuStatistiques
+     */
     private VueMenuStatistiques vueMenuStatistiques = null;
+
+    /**
+     * <p>
+     *     Menu permettant de rechercher des types de batiments
+     * </p>
+     *
+     * @see VueMenuRecherche
+     */
     private VueMenuRecherche vueMenuRecherche = null;
+
+    /**
+     * <p>
+     *     Menu permettant de gérer la culture de l'application
+     * </p>
+     *
+     * @see VueMenuCulture
+     */
     private VueMenuCulture vueMenuCulture = null;
+
+    /**
+     * <p>
+     *     Vue permettant de gérer la population de la planète : villes et avant-postes
+     * </p>
+     *
+     * @see VueMenuPopulation
+     */
     private VueMenuPopulation vueMenuPopulation = null;
+
+    /**
+     * <p>
+     *     Vue permettant de visualiser la carte de la planète
+     * </p>
+     *
+     * @see VueMenuCarte
+     */
     private VueMenuCarte vueMenuCarte  = null;
+
+    /**
+     * <p>
+     *     Vue permettant de gérer les gouverneurs de la planète
+     * </p>
+     *
+     * @see VueMenuGouverneurs
+     */
     private VueMenuGouverneurs vueMenuGouverneurs = null;
+
+    /**
+     * <p>
+     *     vue permettant de gérer un avant-poste
+     * </p>
+     *
+     * @see VueAvantPoste
+     */
     private VueAvantPoste vueAvantPoste = null;
+
+    /**
+     * <p>
+     *     Vue permettant de gérer un gouverneur
+     * </p>
+     *
+     * @see VueGouverneur
+     */
     private VueGouverneur vueGouverneur = null;
+
+    /**
+     * <p>
+     *     Vue permettant d'affecter un gouverneur à une ville
+     * </p>
+     *
+     * @see VueAffecterGouverneur
+     */
     private VueAffecterGouverneur vueAffecterGouverneur = null;
+
+    /**
+     * <p>
+     *     Vue permettant un avant-poste
+     * </p>
+     *
+     * @see VueAjouterAvantPoste
+     */
     private VueAjouterAvantPoste vueAjouterAvantPoste = null;
+
+    /**
+     * <p>
+     *     Vue permettant d'ajouter une mine
+     * </p>
+     *
+     * @see VueAjouterMine
+     */
     private VueAjouterMine vueAjouterMine= null;
+
+    /**
+     * <p>
+     *     Vue permettant de manipuler une ville
+     * </p>
+     *
+     * @see VueVille
+     */
     private VueVille vueVille = null;
+
+    /**
+     * <p>
+     *     Vue permettant  d'ajouter une ville
+     * </p>
+     *
+     * @see VueAjouterVille
+     */
     private VueAjouterVille vueAjouterVille = null;
+
+    /**
+     * <p>
+     *     Vue permettant  d'ajouter un batiment'
+     * </p>
+     *
+     * @see VueAjouterBatiment
+     */
     private VueAjouterBatiment vueAjouterBatiment = null;
 
-
-
+    /**
+     *<p>
+     *     Controleur pricipal de l'application
+     *</p>
+     * @see controler.ControleurPrincipal
+     * */
     private controler.ControleurPrincipal controleur = null;
 
+    /**
+     *<p>
+     *     Unique instance du navigateur des vue. Le navigateur des vues est un singleton
+     *</p>
+     *
+     * */
     static private NavigateurDesVues instance = null;
 
+
+    /**
+     *<p>
+     *     Constructeur de NavigateurDesVues : contruit les différentes vues de l'apllication
+     *</p>
+     *
+     * @see NavigateurDesVues#vueMenuPrincipal
+     * @see NavigateurDesVues#vueMenuStatistiques
+     * @see NavigateurDesVues#vueMenuRecherche
+     * @see NavigateurDesVues#vueMenuCulture
+     * @see NavigateurDesVues#vueMenuPopulation
+     * @see NavigateurDesVues#vueMenuCarte
+     * @see NavigateurDesVues#vueMenuGouverneurs
+     * @see NavigateurDesVues#vueAvantPoste
+     * @see NavigateurDesVues#vueGouverneur
+     * @see NavigateurDesVues#vueAffecterGouverneur
+     * @see NavigateurDesVues#vueAjouterAvantPoste
+     * @see NavigateurDesVues#vueAjouterMine
+     * @see NavigateurDesVues#vueVille
+     * @see NavigateurDesVues#vueAjouterVille
+     * @see NavigateurDesVues#vueAjouterBatiment
+     *
+     * */
     public NavigateurDesVues()
     {
         NavigateurDesVues.instance=this;
@@ -50,11 +222,31 @@ public class NavigateurDesVues extends Application{
         this.vueAjouterBatiment = new VueAjouterBatiment();
     }
 
+    /**
+     *<p>
+     *     Retourne l'unique instance de NavigateurDesVues
+     *</p>
+     *
+     * @return L'unique instance de NavigateurDesVues
+     *
+     * @see NavigateurDesVues#instance
+     *
+     * */
     static public NavigateurDesVues getInstance()
     {
         return instance;
     }
 
+    /**
+     *<p>
+     *     Méthode start permettant de démarrer l'application : intialise le stage de l'application et affecte le controleur aux différentes vues
+     *</p>
+     *
+     * @param stage stage de l'application
+     *
+     * @see NavigateurDesVues#instance
+     *
+     * */
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
@@ -85,137 +277,430 @@ public class NavigateurDesVues extends Application{
         this.vueAjouterBatiment.setControleur(controleur);
     }
 
+    /**
+     *<p>
+     *     Ferme le stage et donc la fenêtre de l'application
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     *
+     * */
     public void fermerFenetre() {
         stage.close();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuPrincipal en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuPrincipal
+     *
+     * */
     public void naviguerVersVueMenuPrincipal()
     {
         stage.setScene(this.vueMenuPrincipal);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuStatistiques en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuStatistiques
+     *
+     * */
     public void naviguerVersMenuStatistiques(){
         stage.setScene(this.vueMenuStatistiques);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuRecherche en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuRecherche
+     *
+     * */
     public void naviguerVersMenuRecherche(){
         stage.setScene(this.vueMenuRecherche);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuPopulation en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuPopulation
+     *
+     * */
     public void naviguerVersMenuPopulation(){
         stage.setScene(this.vueMenuPopulation);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuCulture en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuCulture
+     *
+     * */
     public void naviguerVersMenuCulture(){
         stage.setScene(this.vueMenuCulture);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuCarte en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuCarte
+     *
+     * */
     public void naviguerVersMenuCarte(){
         stage.setScene(this.vueMenuCarte);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueMenuGouverneurs en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueMenuGouverneurs
+     *
+     * */
     public void naviguerVersMenuGouverneurs(){
         stage.setScene(this.vueMenuGouverneurs);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAvantPoste en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public void naviguerVersAvantPoste(){
         stage.setScene(this.vueAvantPoste);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueGouverneur en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueGouverneur
+     *
+     * */
     public void naviguerVersGouverneur() {
         stage.setScene(this.vueGouverneur);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAffecterGouverneur en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAffecterGouverneur
+     *
+     * */
     public void naviguerVersAffecterGouverneur(){
         stage.setScene(this.vueAffecterGouverneur);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAjouterAvantPoste en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAjouterAvantPoste
+     *
+     * */
     public void naviguerVersAjouterAvantPoste(){
         stage.setScene(this.vueAjouterAvantPoste);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAjouterMine en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAjouterMine
+     *
+     * */
     public void naviguerVersVueAjouterMine() {
         stage.setScene(this.vueAjouterMine);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueVille en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueVille
+     *
+     * */
     public void naviguerVersVueVille() {
         stage.setScene(this.vueVille);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAjouterVille en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAjouterVille
+     *
+     * */
     public void naviguerVersVueAjouterVille() {
         stage.setScene(this.vueAjouterVille);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     met la vueAjouterBatiment en tant que scene du stage pour l'afficher.
+     *</p>
+     *
+     * @see NavigateurDesVues#stage
+     * @see NavigateurDesVues#vueAjouterBatiment
+     *
+     * */
     public void naviguerVersVueAjouterBatiement() {
         stage.setScene(this.vueAjouterBatiment);
         stage.show();
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueAvantPoste getVueAvantPoste() {
         return vueAvantPoste;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueGouverneur getVueGouverneur() {
         return vueGouverneur;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueAffecterGouverneur getVueAffecterGouverneur(){ return vueAffecterGouverneur;}
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueAjouterAvantPoste getVueAjouterAvantPoste() {
         return vueAjouterAvantPoste;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueMenuPrincipal getVueMenuPrincipal() {
         return vueMenuPrincipal;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueMenuRecherche getVueMenuRecherche() {
         return vueMenuRecherche;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueMenuCulture getVueMenuCulture() {
         return vueMenuCulture;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueMenuPopulation getVueMenuPopulation() {
         return vueMenuPopulation;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAvantPoste
+     *</p>
+     *
+     * @return la VueAvantPoste
+     *
+     * @see NavigateurDesVues#vueAvantPoste
+     *
+     * */
     public VueMenuCarte getVueMenuCarte() {
         return vueMenuCarte;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueMenuGouverneurs
+     *</p>
+     *
+     * @return la VueMenuGouverneurs
+     *
+     * @see NavigateurDesVues#vueMenuGouverneurs
+     *
+     * */
     public VueMenuGouverneurs getVueMenuGouverneurs() {
         return vueMenuGouverneurs;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueMenuStatistiques
+     *</p>
+     *
+     * @return la VueMenuStatistiques
+     *
+     * @see NavigateurDesVues#vueMenuStatistiques
+     *
+     * */
     public VueMenuStatistiques getVueMenuStatistiques() {
         return vueMenuStatistiques;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAjouterMine
+     *</p>
+     *
+     * @return la VueAjouterMine
+     *
+     * @see NavigateurDesVues#vueAjouterMine
+     *
+     * */
     public VueAjouterMine getVueAjouterMine() {
         return this.vueAjouterMine;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueVille
+     *</p>
+     *
+     * @return la VueVille
+     *
+     * @see NavigateurDesVues#vueVille
+     *
+     * */
     public VueVille getVueVille() {
         return this.vueVille;
     }
 
+    /**
+     *<p>
+     *     Retourne la VueAjouterVille
+     *</p>
+     *
+     * @return la VueAjouterVille
+     *
+     * @see NavigateurDesVues#vueAjouterVille
+     *
+     * */
     public VueAjouterVille getVueAjouterVille() {return this.vueAjouterVille;}
 
+    /**
+     *<p>
+     *     Retourne la VueAjouterBatiment
+     *</p>
+     *
+     * @return la VueAjouterBatiment
+     *
+     * @see NavigateurDesVues#vueAjouterBatiment
+     *
+     * */
     public VueAjouterBatiment getVueAjouterBatiment() {return this.vueAjouterBatiment;}
 }
